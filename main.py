ee3be9a1849ba78690620a2212bc708796a0f41c
Kamal Sharma <sharma.kamal.design@gmail.com>
2026-07-17 13:28:39 +0530

    remove static

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from fastapi.templating import Jinja2Templates
 from openai_service import extract_invoice_data
 
 app = FastAPI(title="Image OCR Demo")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
